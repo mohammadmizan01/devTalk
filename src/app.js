81c4b8195ca4532966d94cc223673e668ff62a8b
mohammadmizna01 <mohammadmizan14257@gmail.com>
2026-01-08 14:07:48 +0530

    making dummy routes

--- src/app.js
+++ src/app.js
@@ -2,13 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req,res) =>{
-    res.end("response from server response");
-})
 
-app.use("/test", (req,res) =>{
+app.post("/user",(req,res,next) =>{
+    console.log('first');
+    // res.send({firstName: "Mohammad", lastName:"Mizan"});
+    next();
+},
+(req,res,next) => {
+    console.log('2nd');
+    // res.send({firstName: "none", lastName:"Mizan"});
+    next();
+});
+
+app.get("/test", (req,res) =>{
     res.end("test page");
-})
+});
+
+// app.use("/", (req,res) =>{
+//     res.end("response from server response");
+// });
 
 
 app.listen(7000,()=>{
